495b0461ff61cac4c7af96bfd313e30aa87401b5
gab <gabrielbordeaux@gmail.com>
2017-07-12 11:49:24 -0400

    Add manifest

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ try:
 except(IOError, ImportError):
     long_description = open('README.md').read()
 
-setup (
-    name = 'pg_batch',
-    version = '1.0.4',
-    description = 'Run large PostgreSQL UPDATE and DELETE queries with small batches to prevent locks',
-    long_description = long_description,
-    author = 'Gabriel Bordeaux',
-    author_email = 'pypi@gab.lc',
-    url = 'https://github.com/gabfl/pg-batch',
-    license = 'MIT',
-    packages = ['pg_batch'],
-    package_dir = { 'pg_batch': 'src' },
-    install_requires = ['psycopg2', 'argparse'], # external dependencies
-    entry_points = {
+setup(
+    name='pg_batch',
+    version='1.0.5',
+    description='Run large PostgreSQL UPDATE and DELETE queries with small batches to prevent locks',
+    long_description=long_description,
+    author='Gabriel Bordeaux',
+    author_email='pypi@gab.lc',
+    url='https://github.com/gabfl/pg-batch',
+    license='MIT',
+    packages=['pg_batch'],
+    package_dir={'pg_batch': 'src'},
+    install_requires=['psycopg2', 'argparse'],  # external dependencies
+    entry_points={
         'console_scripts': [
             'pg_batch = pg_batch.pg_batch:main',
         ],
